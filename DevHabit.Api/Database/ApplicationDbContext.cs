245d31425afe2c19dd7cb1dd2da31f7b5af6086f
Shayne Meyer <shayne.meyer@gmail.com>
2025-11-21 16:11:40 -0800

    added container support and added postgres database

---
+++ DevHabit.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using DevHabit.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Habit> Habits { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Application);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
--- DevHabit.Api/Program.cs
+++ DevHabit.Api/Program.cs
@@ -1,3 +1,8 @@
+using DevHabit.Api.Database;
+using DevHabit.Api.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -11,11 +16,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("Database"),
+        npgsqlOptions => npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+        .UseSnakeCaseNamingConvention()
+);
+
 builder.Services.AddOpenTelemetry()
 .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
 .WithTracing(tracing => tracing
     .AddHttpClientInstrumentation()
-    .AddAspNetCoreInstrumentation())
+    .AddAspNetCoreInstrumentation()
+    .AddNpgsql())
 .WithMetrics(metrics => metrics
     .AddHttpClientInstrumentation()
     .AddAspNetCoreInstrumentation()
@@ -34,6 +47,8 @@ WebApplication app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    await app.ApplyMigrationsAsync();
 }
 
 app.UseHttpsRedirection();
